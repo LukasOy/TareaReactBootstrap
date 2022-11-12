4e40b043fd8d362ed7b6acadf15f7357712eb494
LukasOy <lukas.oylu@gmail.com>
2022-11-12 14:54:16 +0000

    primer guardado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import CollapsibleExample from "./navbar.jsx";
+import JumbotronExample from "./jumbotron.jsx";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -7,20 +9,12 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<CollapsibleExample/>
+			<JumbotronExample/>
+
 		</div>
 	);
 };
+
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,7 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+import CollapsibleExample from "../js/component/navbar.jsx"
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
